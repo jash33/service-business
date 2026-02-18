451117f242246e0e7e129f865bec6a238bc8aad3
James Ashley <james33ashley@gmail.com>
2026-02-18 00:13:03 -0600

    test commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   // IMPORTANT: Set this to your actual production domain
-  site: "https://jash33.github.io", // TODO: Set this to your actual production domain
+  site: "https://jash33.github.io", // TODO: Set this to your actual production domain!
   base: "/service-business",
 
   // Build optimizations
